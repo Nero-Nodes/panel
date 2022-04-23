27594e9d73a793837c7602b9f79348a09f4f7eec
cmrxnn <cmrxn2005@gmail.com>
2022-04-23 21:11:00 +0000

    Change migration name

--- database/migrations/2022_04_23_164347_add_logins_to_users_table.php
+++ database/migrations/2022_04_23_164347_add_logins_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddInitToUsersTable extends Migration
+class AddLoginsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
