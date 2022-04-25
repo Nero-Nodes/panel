10d78477e6fc20252cf890e3816485a0046bfb54
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-04-25 01:07:42 +0100

    Fix discord configuration file

--- config/discord.php
+++ config/discord.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'client_id' => '',
-    'client_secret' => '',
-    'bot_token' => '',
-    'redirect_url' => ''
+    'client_id' => env('DISCORD_CLIENT_ID', false),
+    'client_secret' => env('DISCORD_CLIENT_SECRET', false),
+    'bot_token' => env('DISCORD_BOT_TOKEN', false),
+    'redirect_url' => env('DISCORD_REDIRECT_URL', false),
 ];
