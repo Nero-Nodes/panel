7537f9bff26d899a0eb8dd0134fbf6c6bfd77ccf
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-05-02 00:28:59 +0100

    Fix API route for Discord authentication

--- resources/scripts/api/auth/discord.ts
+++ resources/scripts/api/auth/discord.ts
@@ -2,7 +2,7 @@ import http from '@/api/http';
 
 export default (): Promise<any> => {
     return new Promise((resolve, reject) => {
-        http.post('/auth/discord')
+        http.post('/auth/discord/login')
             .then((data) => resolve(data.data || []))
             .catch(reject);
     });
