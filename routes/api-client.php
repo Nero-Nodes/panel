a6478a7ef96d5567c412bad1afa01fb150f71c31
cmrxnn <cmrxn2005@gmail.com>
2022-05-09 11:46:51 +0000

    Allow server deletion when server is suspended

--- routes/api-client.php
+++ routes/api-client.php
@@ -128,7 +128,7 @@ Route::group(['prefix' => '/servers/{server}', 'middleware' => [AuthenticateServ
 
     Route::group(['prefix' => '/settings'], function () {
         Route::post('/rename', 'Servers\SettingsController@rename');
-        Route::post('/delete', 'Credits\StoreController@deleteServer');
+        Route::post('/delete', 'Credits\StoreController@deleteServer')->name('api:client:server.delete');
         Route::post('/reinstall', 'Servers\SettingsController@reinstall');
         Route::post('/edit', 'Servers\SettingsController@edit');
         Route::put('/docker-image', 'Servers\SettingsController@dockerImage');
