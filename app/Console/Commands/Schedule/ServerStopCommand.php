0f437b84c63c1097b2e916748c0fc1495ab96ec1
cmrxnn <cmrxn2005@gmail.com>
2022-05-15 16:15:12 +0000

    Streamline server shutdown script

--- app/Console/Commands/Schedule/ServerStopCommand.php
+++ app/Console/Commands/Schedule/ServerStopCommand.php
@@ -59,36 +59,21 @@ class ServerStopCommand extends Command
                 continue;
             }
 
-            // Log to the console when a server has been detected as having
-            // only the default limits allocated to it.
-            $this->output($svr->id.' | '.$svr->status.' | Detected as having '.$svr->cpu.'% CPU, '.$svr->memory.'MB RAM, '.$svr->disk.'MB disk.', false);
-
             // Send a message to the console which will show up in Minecraft
             // servers which informs the user of the scheduled shutdown.
             try {
                 $commandRepository->setServer($svr)->send('say This server is being shut down due to you using the Free Tier, meaning you haven\'t upgraded your server instance. You can restart your server at any time. Please consider upgrading your instance via the Store on the control panel.');
             } catch (DaemonConnectionException $exception) {
-                // Don't move on if this fails, server could still be available.
-                $this->output($svr->id.' | ERR | Unable to send message to server console. ', false);
+                // If we're unable to send the message, who cares.
             }
 
-            // Sleep for 5 seconds to allow for users to read message.
-            $this->output($svr->id.' | Waiting for 5 seconds until shutdown...', false);
-            sleep(5);
-
             try {
-                // If the server is already offline, don't try and shut it down.
-                if ($svr->status == 'offline') {
-                    $this->output($svr->id.' | Server is already offline, ignoring.', false);
-                    continue;
-                }
-
                 // If the status is not offline or suspended, shut down the server instance.
                 $powerRepository->setServer($svr)->send('stop');
-                $this->output($svr->id . ' | Shutdown success, looping to next server.', false);
+                $this->output($svr->id . ' | Shutdown success', false);
             } catch (DaemonConnectionException $exception) {
                 // Report an error to the console when server cannot be shutdown.
-                $this->output($svr->id.' | ERR | Unable to connect to the daemon, unable to request server shutdown.', false);
+                $this->output($svr->id.' | ERR | Unable to connect to the daemon', false);
                 continue;
             }
         };
